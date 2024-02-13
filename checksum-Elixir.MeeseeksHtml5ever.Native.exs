4b9ea93427e7ffea1fd82d310ec594535420aa53
lud-wj <ludovic.demblans@wttj.co>
2024-02-13 15:29:33 +0100

    chore: publish the checksums

---
+++ checksum-Elixir.MeeseeksHtml5ever.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:231045309cce00c05b7b7059c784f414ec8ab936d3afd9c4e22f498b1475cdcf",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e71b271bea6c2559e139151fc4a50dc0c5cc06ed091ffe15c2f352946385260e",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:76038dc2b1fbe4cec2f501f894bff68e22ddd227d1a7937e2d91ad1b17fa42ae",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:0bf57ebb1de70ea1a18c840333ea2112f8a4965c62806aa208fbd02fc6875f0b",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:f4484927a4da5253cda875d84f254bfdffb7e84792060cef87b0b26924703d3c",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:55547394e1749a86cab732fb1b2dc07a942d3626e96bf5455b43cbb15d15fbb2",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:6f990b01a0381ede5c99c43c1d3be9a4fd4effab2928e330bf6248797b98fbb9",
+  "libmeeseeks_html5ever_nif-v0.14.3-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:0a5b3dd65e6effef808a28b5420a7b4153b8a1f37cf66e48e7d8ef78ffede823",
+  "meeseeks_html5ever_nif-v0.14.3-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:1b09f0a45e10d3f8d2770e1a0555f9080a31bddcaaa9b076b13d71f8aabcc0ab",
+  "meeseeks_html5ever_nif-v0.14.3-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:9077a9958e48d4810f7c1a6d477baefa94820623d15c036f3b2b12b3321ef55e",
+}
